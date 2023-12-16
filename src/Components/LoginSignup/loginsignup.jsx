9f50f50a5b56360f44772bb1f10dc06872dde91a
viveksworkspace <teamadsdev22@gmail.com>
2023-12-16 15:29:15 +0530

    s

---
+++ src/Components/LoginSignup/loginsignup.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './loginsignup.css'
+import user_icon from '../Assets/person.png'
+import password_icon from '../Assets/password.png'
+import email_icon from '../Assets/email.png'
+export const loginsignup = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className="forgot-password">Lost Password <span>Click Here!</span></div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Login</div>
+      </div>
+    </div>
+  );
+};
+
+
+export default loginsignup; 
